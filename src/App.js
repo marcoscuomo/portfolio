d14500a3ae5d2f06c8f7b65084779407bf8bf883
Marcos Souza <bytecore@gmail.com>
2021-11-08 21:54:05 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import NavMenu from './components/NavMenu';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+
+export default function App() {
+  return (
+    <>
+      <Router>
+        <NavMenu />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
+}
